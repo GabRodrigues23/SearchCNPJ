886ee7d3cbf3ec7201110d6aba45946a563cb8c4
gabriel.rodrigues <gabrigues23@gmail.com>
2025-04-30 14:52:05 -0300

    Change package FPHTTPClient to Synapse

--- umain.pas
+++ umain.pas
@@ -5,7 +5,7 @@ unit uMain;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, opensslsockets, fphttpclient, fpjson, jsonparser;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, httpsend, ssl_openssl, fpjson, jsonparser;
 
 type
 
@@ -19,8 +19,6 @@ type
     btnSearch: TButton;
     memo: TMemo;
     procedure btnSearchClick(Sender: TObject);
-    procedure Button1Click(Sender: TObject);
-
   private
 
   public
@@ -36,62 +34,63 @@ implementation
 
 { TfrmMain }
 
+function GetSafeJSONValue(JSON: TJSONData; const Path, Default: string): string;
+var
+  Node: TJSONData;
+begin
+  Node := JSON.FindPath(Path);
+  if Assigned(Node) then
+    Result := Node.AsString
+  else
+    Result := Default;
+end;
+
 procedure TfrmMain.btnSearchClick(Sender: TObject);
 var
-  CNPJ, URL, Response: string;
-  HTTPClient: TFPHTTPClient;
-  JSON: TJSONData;
+  CNPJ, URL, Response : String;
+  HTTP : THTTPSend;
+  ResponseStream : TStringStream;
+  JSON : TJSONdata;
+
 begin
   try
     CNPJ := txtCNPJ.Text;
-
     URL := 'https://receitaws.com.br/v1/cnpj/' + CNPJ;
+    JSON := nil;
+
+    Memo.Lines.Clear;
+
+    HTTP := THTTPSend.Create;
+    ResponseStream := TStringStream.Create('');
+
     try
-      HTTPClient := TFPHTTPClient.Create(nil);
+      if HTTP.HTTPMethod('GET', URL) then
+        begin
+          ResponseStream.LoadFromStream(HTTP.Document);
+          Response := ResponseStream.DataString;
 
-      Response := HTTPClient.Get(URL);
-      JSON := GetJSON(Response);
+          JSON := GetJSON(Response);
 
-      memo.Lines.Add('Razão Social: ' + JSON.FindPath('nome').AsString);
-      memo.Lines.Add('Nome Fantasia: ' +  JSON.FindPath('fantasia').AsString);
-      memo.Lines.Add('CNPJ: ' + JSON.FindPath('cnpj').AsString);
-
-      if JSON.FindPath('ie') <> nil then
-        memo.Lines.Add('Inscrição Estadual: ' + JSON.FindPath('ie').AsString)
+          memo.Lines.Add('Razão Social: ' + JSON.FindPath('nome').AsString);
+          memo.Lines.Add('Nome Fantasia: ' +  JSON.FindPath('fantasia').AsString);
+          memo.Lines.Add('CNPJ: ' + JSON.FindPath('cnpj').AsString);
+          memo.Lines.Add('Inscrição Estadual: ' + GetSafeJSONValue(JSON, 'ie', '[N/A]'));
+          memo.Lines.Add('Endereço: ' + JSON.FindPath('logradouro').AsString + ', '
+                        + JSON.FindPath('numero').AsString + ' - '
+                        + JSON.FindPath('bairro').AsString + ' - '
+                        + JSON.FindPath('municipio').AsString + ' - '
+                        + JSON.FindPath('uf').AsString);
+        end
       else
-        memo.Lines.Add('Inscrição Estadual: [N/A]');
-
-
-      memo.Lines.Add('Endereço: ' + JSON.FindPath('logradouro').AsString + ', '
-                    + JSON.FindPath('numero').AsString + ' - '
-                    + JSON.FindPath('bairro').AsString + ' - '
-                    + JSON.FindPath('municipio').AsString + ' - '
-                    + JSON.FindPath('uf').AsString);
+        ShowMessage('Erro ao conectar na API');
     except
-      on E: Exception do
-        ShowMessage('Erro: ' + E.Message);
+      on e: Exception do
+        ShowMessage('Erro' + e.Message);
     end;
   finally
-    HTTPClient.Free;
+    JSON.Free;
+    ResponseStream.Free;
+    HTTP.Free;
   end;
-
-
 end;
-
-procedure TfrmMain.Button1Click(Sender: TObject);
-var
-  client: TFPHTTPClient;
-  response: String;
-begin
-  client := TFPHTTPClient.Create(nil);
-  try
-    response := client.Get('https://www.google.com');
-    ShowMessage('Funcionou!');
-  except
-    on E: Exception do
-      ShowMessage('Erro: ' + E.Message);
-  end;
-  client.Free;
-end;
-
 end.
